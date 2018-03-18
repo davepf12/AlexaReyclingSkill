8250221fe289476b6e3d442c2f3d6f2438ec44d6
David Fellows <davepf12@hotmail.com>
2018-03-18 19:44:15 +0000

    First piece of code
    This is the first piece of code for our Alexa Skill

---
+++ AlexaCode.js
@@ -0,0 +1,13 @@
+// Alexa 
+var AlexaResponseTrash = '2am on a Monday ';
+var AlexaSentanceTrash = 'Your Trash will be collected at  '
+var AlexaResponseRecycling = 'Tuesday at 6 PM '
+var AlexaSentanceRecycling = 'Your Recycling will be collected on ' 
+console.log(AlexaSentanceTrash + AlexaResponseTrash);
+console.log(AlexaSentanceRecycling + AlexaResponseRecycling);
+
+
+
+
+
+
